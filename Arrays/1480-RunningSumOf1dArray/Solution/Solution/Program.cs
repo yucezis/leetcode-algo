3cc97ba7a4e06ee7a1a56922f2b8dac5557ac3f2
z <yucezisan@gmail.com>
2026-03-05 11:42:59 +0300

    feat: add console app for LeetCode 1480 Running Sum of 1d Array

---
+++ Arrays/1480-RunningSumOf1dArray/Solution/Solution/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LeetCodeRunningSum
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+            {
+                nums[i] = nums[i] + nums[i - 1];
+            }
+            return nums;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int[] testDizisi = { 1, 2, 3, 4 };
+
+            Console.WriteLine("Girdi Dizisi:");
+            Console.WriteLine("[ " + string.Join(", ", testDizisi) + " ]");
+
+            int[] sonucDizisi = solution.RunningSum(testDizisi);
+
+            Console.WriteLine("\nÇalışan Toplam (Running Sum) Çıktısı:");
+            Console.WriteLine("[ " + string.Join(", ", sonucDizisi) + " ]");
+
+            Console.WriteLine("\nÇıkmak için bir tuşa basın...");
+            Console.ReadKey();
+        }
+    }
+}
